91d497196ce55a30421728bd5eb2e47435bdc1a5
Ramel Empleo <84055531+Ramel-Alphasys@users.noreply.github.com>
2022-11-10 23:23:42 +0800

    transtion updates

--- glamserver/assets/php/transactionCRUD.php
+++ glamserver/assets/php/transactionCRUD.php
@@ -1,1 +1,18 @@
 <?php
+require 'database.php';
+require 'CRUD.php';
+
+$crud = new serverManipulation();
+// set here connection to database
+$conToServer = new ServerCon(['localhost', 3306, 'glamdb', $user, $pass]);
+
+if (!empty($_POST['TYPE'])) {
+    switch ($_POST['TYPE']) {
+        case 'updateTransaction':
+            try {
+
+            } catch (PDOException $e) {
+                echo json_encode([['MESSAGE' => "Connection failed in updateProduct: " . $conToServer->htmlize($e->getMessage())]]);
+            }
+    }
+}
