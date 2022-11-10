ce918e7cd0b9a9b803e59704ed9dfe397237a0c0
Methz <methsdev@alphasys.com.au>
2022-11-10 23:07:05 +0800

    Detail And Product

--- index.php
+++ index.php
@@ -39,13 +39,13 @@
           <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
         </div>
         <div class="carousel-inner">
-          <div class="carousel-item active">
+          <div class="carousel-item active" data-bs-interval="3000">
             <img src="assets/img/img2.jpg" class="d-block w-100 img-fluid" alt="...">
           </div>
-          <div class="carousel-item">
+          <div class="carousel-item" data-bs-interval="3000">
             <img src="assets/img/imgPlace.jpg" class="d-block w-100 img-fluid" alt="...">
           </div>
-          <div class="carousel-item">
+          <div class="carousel-item" data-bs-interval="3000">
             <img src="assets/img/img3.jpg" class="d-block w-100 img-fluid" alt="...">
           </div>
         </div>
