1d619f7b270750f9416abb7863237cddb25a4592
Methsdev Tambiga <methsdev@alphasys.com.au>
2022-11-23 21:35:30 +0800

    save profile

--- assets/php/updateInfo.php
+++ assets/php/updateInfo.php
@@ -4,19 +4,19 @@
      require '..\..\glamserver\assets\php\CRUD.php';
      $crud = new serverManipulation();
      $serverConn  = new ServerCon(['localhost', 3306, 'glamdb', 'Metx', '(sX6j8k_GQ07uw*1']);
-
+     $updata = " `gc_gname` = '".$_POST["gname"]."', `gc_sname` = '".$_POST["sname"]."', `gc_p_m_number` = '".$_POST["mobile"]."', `gc_email` = '".$_POST["email"]."', `gc_address` = '".$_POST["street"].", ". $_POST["brgy"].", ".$_POST["city"].", ".$_POST["province"]."' WHERE `g_customer`.`gcId` =".$_POST["gc_id"]." ";
+     
      if(isset($_POST['submit'])) {
+          
+          print_r($updata);
           $params = array(
-               "fields" => 
-               "`gc_gname`='"+$_POST["gname"]+
-               "', `gc_sname`='"+$_POST["sname"]+
-               "', `gc_p_m_number`='"+$_POST["mobile"]+
-               "', `gc_email`='"+$_POST["email"]+
-               "', `gc_address`='"+$_POST["street"]+", "+ $_POST["brgy"]+", "+$_POST["city"]+", "+$_POST["province"],
+               "fields" => $updata,
                "table" => 'g_customer',
                "dbcon" => $serverConn
           );
 
           $response = $crud->sm_ur_server($params);
+          print_r($response);
      }
+     header('Location: ..\..\Profile.php');
 
