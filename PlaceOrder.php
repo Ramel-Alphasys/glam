99812b08b135fd6e12aadfce562063c140760ef7
Methsdev Tambiga <methsdev@alphasys.com.au>
2022-12-17 10:56:59 +0800

    Update PlaceOrder.php

--- PlaceOrder.php
+++ PlaceOrder.php
@@ -18,7 +18,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Product</title>
+    <title>Checkout</title>
     <link href="assets/css/bootstrap.min.css" rel="stylesheet">
     <script src="assets/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
