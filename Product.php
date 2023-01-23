870783bde8609276672fec746446f1926872740f
Methz <methsdev@alphasys.com.au>
2023-01-23 23:29:42 +0800

    Update Product.php

--- Product.php
+++ Product.php
@@ -12,7 +12,6 @@ require 'glamserver\assets\php\CRUD.php';
 $crud = new serverManipulation();
 $serverConn  = new ServerCon(['localhost', 3306, 'glamdb', 'glam', '-TEnT3pf_-JqPbX*']);
 
-$sorting = '';
 if(isset($_GET['sort'])){
   $sorting = $_GET['sort'];
   $params = array(
